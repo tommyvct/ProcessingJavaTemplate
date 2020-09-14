52f0309e39ad2bcca1ac624074ada6d19d20285d
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2020-09-13 19:37:54 -0500

    init

---
+++ src/App.java
@@ -0,0 +1,24 @@
+import processing.core.*;
+
+public class App extends PApplet 
+{
+    public static void main(String[] args) 
+    {
+        PApplet.runSketch(new String[]{"App"}, new App());
+    }
+
+    public void settings()
+    {
+        size(500, 500);
+    }
+
+    public void setup()
+    {
+
+    } 
+
+    public void draw()
+    {
+
+    }
+}
