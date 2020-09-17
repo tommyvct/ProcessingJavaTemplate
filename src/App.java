5fc288a4a52ad9552ed0896a971608e84060405e
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2020-09-16 23:31:31 -0500

    ref

--- src/App.java
+++ src/App.java
@@ -8,32 +8,27 @@ public class App extends PApplet {
      * <h1>Entry point</h1>
      * 
      * The {@code appletArgs} must match this class's name
+     * 
      * @param passedArgs
      */
-    static public void main(String[] passedArgs) 
-    {
+    static public void main(String[] passedArgs) {
         String[] appletArgs = new String[] { "App" };
-        
-        if (passedArgs != null) 
-        {
-          PApplet.main(concat(appletArgs, passedArgs));
-        } 
-        else 
-        {
-          PApplet.main(appletArgs);
+
+        if (passedArgs != null) {
+            PApplet.main(concat(appletArgs, passedArgs));
+        } else {
+            PApplet.main(appletArgs);
         }
     }
-    
-    public void settings() 
-    {
+
+    public void settings() {
         size(640, 640, P3D);
     }
 
-    public void setup() 
-    {
+    public void setup() {
     }
 
-    public void draw() 
-    {
+    public void draw() {
+
     }
 }
